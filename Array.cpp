1619a7880896530cb662df57f0702542414bc79a
kunal <kunal20vijay00@gmail.com>
2021-09-14 13:22:17 +0530

    Array Insertion Modified

--- Array.cpp
+++ Array.cpp
@@ -1,99 +1,95 @@
 #include <iostream>
 using namespace std;
-
 class myArray
 {
-public:
+  public:
     int *total_size = new int;
     int *used_size = new int;
     int *arr = new int[*total_size];
     myArray(void);
-    void printArray(myArray);
-    void insert(myArray);
-    void deletion(myArray);
+    void printArray();
+    void insert();
+    void deletion();
 };
-
-//Array creation
 myArray::myArray(void)
 {
-    int t_size, u_size;
-    cout << "Enter total size and size you want to use:";
-    cin >> t_size >> u_size;
+  int t_size, u_size;
+  cout << "Enter total size and size you want to use:";
+  cin >> t_size >> u_size;
 
-    *total_size = t_size;
-    *used_size = u_size;
+  *total_size = t_size;
+  *used_size = u_size;
 
-    cout << "Enter the array space separated" << endl;
-    for (int i = 0; i < *used_size; i++)
-    {
-        cin >> arr[i];
-    }
+  cout << "Enter the array space separated" << endl;
+  for (int i = 0; i < *used_size; i++)
+  {
+    cin >> arr[i];
+  }
 }
 
-//Array print
-void myArray::printArray(myArray a)
+void myArray::printArray()
 {
-    cout << "Array:";
-    for (int i = 0; i < *a.used_size; i++)
-    {
-        cout << a.arr[i] << " ";
-    }
-    cout << endl;
+  cout << "Array:";
+  for (int i = 0; i < *used_size; i++)
+  {
+    cout << arr[i] << " ";
+  }
+  cout << endl;
 }
 
 //Insertion in Array
-void myArray::insert(myArray a)
+void myArray::insert()
 {
-    a.printArray(a);
-    int x;
-    cout << "Enter number to insert:";
-    cin >> x;
-    if (*a.total_size > *a.used_size)
+  int x;
+  cout << "Enter number to insert:";
+  cin >> x;
+  if (*total_size > *used_size)
+  {
+    arr[*(used_size)] = x;
+    *used_size=*used_size+1;
+  cout <<"\n used size :" ;
+  }
+  else
+  {
+    int *temp = new int[*(total_size)];
+    for (int i = 0; i < *total_size; i++)
     {
-        a.arr[*(a.used_size)] = x;
-        //    *a.used_size=*a.used_size+1;
+      temp[i] = arr[i];
     }
-    else
+    ++(*(total_size));
+    arr = new int[*(total_size)];
+    for (int i = 0; i < *total_size - 1; i++)
     {
-        int *temp = new int[*(a.total_size)];
-        for (int i = 0; i < *a.total_size; i++)
-        {
-            temp[i] = a.arr[i];
-        }
-        ++(*(a.total_size));
-        a.arr = new int[*(a.total_size)];
-        for (int i = 0; i < *a.total_size - 1; i++)
-        {
-            a.arr[i] = temp[i];
-        }
-        a.arr[(*(a.total_size))-1] = x;
-        delete temp;
+      arr[i] = temp[i];
     }
-    ++(*(a.used_size));
+    arr[(*(total_size))-1] = x;
+    delete temp;
+  }
+  ++(*(used_size));
 }
 //Deletion in array
-void myArray::deletion(myArray a)
+void myArray::deletion()
 {
-    int index;
-    cout << "Enter the index of element you want to delete :";
-    cin >> index;
-    for (int i = index; i < *a.used_size - 1; i++)
-    {
-        a.arr[i] = a.arr[i + 1];
-    }
-    a.arr[*a.used_size - 1] = NULL;
-    --(*(a.used_size));
+  int index;
+  cout << "Enter the index of element you want to delete :";
+  cin >> index;
+  for (int i = index; i < *used_size - 1; i++)
+  {
+    arr[i] = arr[i + 1];
+  }
+  arr[*used_size - 1] = NULL;
+  --(*(used_size));
 }
 
 int main()
 {
-    myArray ar;
-    ar.printArray(ar);
-    ar.insert(ar);
-    ar.printArray(ar);
-    ar.insert(ar);
-    ar.printArray(ar);
-    ar.deletion(ar);
-    ar.printArray(ar);
-    return 0;
+  myArray ar;
+  ar.printArray();
+  ar.insert();
+  ar.printArray();
+  ar.insert();
+  ar.printArray();
+  ar.deletion();
+  ar.printArray();
+  return 0;
 }
