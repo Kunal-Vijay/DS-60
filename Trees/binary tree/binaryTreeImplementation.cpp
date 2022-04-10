10f0921c348cdf5444f84f57d87e83814f88edaa
Kunal <kunal20vijay00@gmail.com>
2022-04-10 13:55:51 +0530

    BT construct - using in and pre order

--- Trees/binary tree/binaryTreeImplementation.cpp
+++ Trees/binary tree/binaryTreeImplementation.cpp
@@ -107,32 +107,72 @@ void printBTlevelWise(BinaryTreenode<int> *root)
     }
 }
 
-void inorder(BinaryTreenode<int>*root){
-    if (root==NULL)
+void inorder(BinaryTreenode<int> *root)
+{
+    if (root == NULL)
     {
         return;
     }
     inorder(root->left);
-    cout<<root->data<<" ";
+    cout << root->data << " ";
     inorder(root->right);
 }
-void preorder(BinaryTreenode<int>*root){
-    if (root==NULL)
+void preorder(BinaryTreenode<int> *root)
+{
+    if (root == NULL)
     {
         return;
     }
-    cout<<root->data<<" ";
+    cout << root->data << " ";
     preorder(root->left);
     preorder(root->right);
 }
-void postorder(BinaryTreenode<int>*root){
-    if (root==NULL)
+void postorder(BinaryTreenode<int> *root)
+{
+    if (root == NULL)
     {
         return;
     }
     postorder(root->left);
     postorder(root->right);
-    cout<<root->data<<" ";
+    cout << root->data << " ";
+}
+
+BinaryTreenode<int> *buildTreeHelper(int *in, int *pre, int inS, int inE, int preS, int preE)
+{
+    if (inS > inE)
+    {
+        return NULL;
+    }
+
+    int rootData = pre[preS];
+    int rootIndex = -1;
+    for (int i = inS; i <= inE; i++)
+    {
+        if (in[i] == rootData)
+        {
+            rootIndex = i;
+            break;
+        }
+    }
+
+    int lPreS = preS + 1;
+    int lInS = inS;
+    int lInE = rootIndex - 1;
+    int lPreE = lInE - lInS + lPreS;
+    int rPreS = lPreE + 1;
+    int rPreE = preE;
+    int rInS = rootIndex + 1;
+    int rInE = inE;
+    BinaryTreenode<int> *root = new BinaryTreenode<int>(rootData);
+    root->left = buildTreeHelper(in, pre, lInS, lInE, lPreS, lPreE);
+    root->right = buildTreeHelper(in, pre, rInS, rInE, rPreS, rPreE);
+    return root;
+}
+
+BinaryTreenode<int> *buildTree(int *in, int *pre, int size)
+{
+    return buildTreeHelper(in, pre, 0, size - 1, 0, size - 1);
 }
 int main()
 {
@@ -142,14 +182,16 @@ int main()
     // root->left = node1;
     // root->right = node2;
     // BinaryTreenode<int> *root = takeInput();
-    BinaryTreenode<int> *root = takeInputLevelWise();
+    // BinaryTreenode<int> *root = takeInputLevelWise();
     // printBinaryTree(root);
-    // printBTlevelWise(root);
-    inorder(root);
-    cout<<endl;
-    preorder(root);
-    cout<<endl;
-    postorder(root);
-    delete root;
+    int in[] = {4, 2, 5, 1, 8, 6, 9, 3, 7};
+    int pre[] = {1, 2, 4, 5, 3, 6, 8, 9, 7};
+    printBTlevelWise(buildTree(in, pre, 9));
+    // inorder(root);
+    // cout << endl;
+    // preorder(root);
+    // cout << endl;
+    // postorder(root);
+    // delete root;
     return 0;
 }
