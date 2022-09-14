2d893f3d0a0f6703f4349acced7703f5cc859b22
Kunal <kunal20vijay00@gmail.com>
2022-09-14 13:40:24 +0530

    Hashmap P1 - Remove duplicates from array

---
+++ Hashmaps/hashmap_problems.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// Problem 1 - Remove Duplicates
+vector<int> removeDuplicates(int *a, int size)
+{
+    vector<int> output;
+    unordered_map<int, bool> seen;
+    for (int i = 0; i < size; i++)
+    {
+        if (seen.count(a[i]) > 0)
+        {
+            continue;
+        }
+        seen[a[i]] = true;
+        output.push_back(a[i]);
+    }
+    return output;
+}
+
+int main()
+{
+    int a[] = {1, 2, 3, 3, 2, 1, 4, 3, 6};
+    vector<int> output = removeDuplicates(a, 9);
+    for (int i = 0; i < output.size(); i++)
+    {
+        cout<<output[i]<<endl;
+    }
+    return 0;
+}
