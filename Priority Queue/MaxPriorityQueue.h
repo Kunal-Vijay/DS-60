c2cb7ef23bf21f60e7f455c3e7ede3dac000902f
Kunal <kunal20vijay00@gmail.com>
2022-09-21 14:14:34 +0530

    Priority Queue Implementation - Max Heap | PQ

---
+++ Priority Queue/MaxPriorityQueue.h
@@ -0,0 +1,90 @@
+#include <vector>
+using namespace std;
+class MaxPriorityQueue
+{
+    vector<int> pq;
+
+public:
+    MaxPriorityQueue()
+    {
+    }
+
+    bool isEmpty()
+    {
+        return pq.size() == 0;
+    }
+
+    int getSize()
+    {
+        return pq.size();
+    }
+
+    int getMax()
+    {
+        if (pq.size() != 0)
+            return pq[0];
+        return 0; // priority queue is empty
+    }
+
+    void insert(int element)
+    {
+        pq.push_back(element);
+
+        // up-heapify
+        int childIndex = pq.size() - 1;
+        while (childIndex > 0)
+        {
+            int parentIndex = (childIndex - 1) / 2;
+            if (pq[parentIndex] < pq[childIndex])
+            {
+                int temp = pq[parentIndex];
+                pq[parentIndex] = pq[childIndex];
+                pq[childIndex] = temp;
+            }
+            else
+            {
+                break;
+            }
+            childIndex = parentIndex;
+        }
+    }
+    int removeMax()
+    {
+        if (isEmpty())
+        {
+            return 0;
+        }
+        int minElement = pq[0];
+        pq[0] = pq[pq.size() - 1];
+        pq.pop_back();
+
+        // down-heapify
+        int parentIndex = 0;
+        int childIndex1 = (2 * parentIndex) + 1;
+        int childIndex2 = (2 * parentIndex) + 2;
+        while (childIndex1 < pq.size())
+        {
+            int maxIndex = parentIndex;
+            if (pq[childIndex1] > pq[maxIndex])
+            {
+                maxIndex = childIndex1;
+            }
+            if (childIndex2 < pq.size() && pq[childIndex2] > pq[maxIndex])
+            {
+                maxIndex = childIndex2;
+            }
+            if (pq[maxIndex] == pq[parentIndex])
+            {
+                break;
+            }
+            int temp = pq[parentIndex];
+            pq[parentIndex] = pq[maxIndex];
+            pq[maxIndex] = temp;
+
+            parentIndex = maxIndex;
+            childIndex1 = (2 * parentIndex) + 1;
+            childIndex2 = (2 * parentIndex) + 2;
+        }
+        return minElement;
+    }
+};
--- Priority Queue/PriorityQueue.cpp
+++ Priority Queue/PriorityQueue.cpp
@@ -1,10 +1,29 @@
 #include <bits/stdc++.h>
 #include "MinPriorityQueue.h"
+#include "MaxPriorityQueue.h"
 using namespace std;
 
 int main()
 {
-    MinPriorityQueue p;
+    // MinPriorityQueue p;
+    // p.insert(100);
+    // p.insert(10);
+    // p.insert(15);
+    // p.insert(4);
+    // p.insert(17);
+    // p.insert(21);
+    // p.insert(67);
+
+    // cout << p.getSize() << endl;
+    // cout << p.getMin() << endl;
+
+    // while (!p.isEmpty())
+    // {
+    //     cout << p.removeMin() << " ";
+    // }
+    // cout<<endl;
+
+    MaxPriorityQueue p;
     p.insert(100);
     p.insert(10);
     p.insert(15);
@@ -14,11 +33,11 @@ int main()
     p.insert(67);
 
     cout << p.getSize() << endl;
-    cout << p.getMin() << endl;
+    cout << p.getMax() << endl;
 
     while (!p.isEmpty())
     {
-        cout << p.removeMin() << " ";
+        cout << p.removeMax() << " ";
     }
     cout<<endl;
     return 0;
