7f61b725d05a74f0933665c6aeb8251193008c60
Kunal <kunal20vijay00@gmail.com>
2023-01-14 17:11:52 +0530

    Graph - BFS print

--- Graph/graph.cpp
+++ Graph/graph.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void print(int **edges, int n, int sv, bool *visited)
+void printDFS(int **edges, int n, int sv, bool *visited) // sv = starting vertex
 {
     cout << sv << endl;
     visited[sv] = true;
@@ -17,28 +17,58 @@ void print(int **edges, int n, int sv, bool *visited)
             {
                 continue;
             }
-            print(edges, n, i, visited);
+            printDFS(edges, n, i, visited);
+        }
+    }
+}
+
+void printBFS(int **edges, int n, int sv)
+{
+    queue<int> pendingVertices;
+    bool *visited = new bool;
+    for (int i = 0; i < n; i++)
+    {
+        visited[i] = false;
+    }
+    pendingVertices.push(sv);
+    visited[sv] = true;
+    while (!pendingVertices.empty())
+    {
+        int currentVertex = pendingVertices.front();
+        pendingVertices.pop();
+        cout << currentVertex << endl;
+        for (int i = 0; i < n; i++)
+        {
+            if (i == currentVertex)
+            {
+                continue;
+            }
+            if (edges[currentVertex][i] == 1 && !visited[i])
+            {
+                pendingVertices.push(i);
+                visited[i] = true;
+            }
         }
     }
 }
 
 int main()
 {
-    int n, e;
-    cin >> n >> e;
-    int **edges = new int *[n];
-    for (int i = 0; i < n; i++)
-    {
-        edges[i] = new int[n];
-        for (int j = 0; j < n; j++)
-        {
-            edges[i][j] = 0;
-        }
+    int n, e;                                                                                                                                                           
+    cin >> n >> e;                                                                                          
+    int **edges = new int *[n]; // edge matrix                                                                                          
+    for (int i = 0; i < n; i++)                                                                                         
+    {                                                                                           
+        edges[i] = new int[n];                                                                                          
+        for (int j = 0; j < n; j++)                                                                                         
+        {                                                                                           
+            edges[i][j] = 0;                                                                                            
+        }                                                                                           
     }
 
     for (int i = 0; i < e; i++)
     {
-        int f, s;
+        int f, s; // first and second vertices
         cin >> f >> s;
         edges[f][s] = 1;
         edges[s][f] = 1;
@@ -50,7 +80,34 @@ int main()
         visited[i] = false;
     }
 
-    print(edges, n, 0, visited);
+    cout<<"DFS"<<endl;
+    printDFS(edges, n, 0, visited);
+    cout<<"BFS"<<endl;
+    printBFS(edges, n, 0);
 
     return 0;
 }
+
+//           0       
+//          / \      
+//         4   5     
+//        /     \    
+//       3-------6   
+//     /  \       \  
+//    2----1       7 
+
+
+//input : 8
+// 9
+// 0 4
+// 0 5
+// 4 3
+// 3 2
+// 2 1
+// 1 3
+// 5 6
+// 3 6
+// 6 7
+
+// DFS = 0 4 3 1 2 6 5 7
+// BFS = 0 4 5 3 6 1 2 7
