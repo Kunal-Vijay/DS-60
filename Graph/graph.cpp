28179e0493a8ad6142d435f727345096797b29ca
Kunal <kunal20vijay00@gmail.com>
2023-01-14 19:50:59 +0530

    Graph - BFS DFS of disconnected graph

--- Graph/graph.cpp
+++ Graph/graph.cpp
@@ -22,14 +22,9 @@ void printDFS(int **edges, int n, int sv, bool *visited) // sv = starting vertex
     }
 }
 
-void printBFS(int **edges, int n, int sv)
+void printBFS(int **edges, int n, int sv, bool *visited)
 {
     queue<int> pendingVertices;
-    bool *visited = new bool;
-    for (int i = 0; i < n; i++)
-    {
-        visited[i] = false;
-    }
     pendingVertices.push(sv);
     visited[sv] = true;
     while (!pendingVertices.empty())
@@ -52,18 +47,54 @@ void printBFS(int **edges, int n, int sv)
     }
 }
 
+// for disconnected graph
+void DFSdisconnected(int **edges, int n)
+{
+    bool *visited = new bool[n];
+    // initializing all nodes univisited
+    for (int i = 0; i < n; i++)
+    {
+        visited[i] = false;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        if (!visited[i])
+        {
+            printDFS(edges, n, i, visited);
+        }
+    }
+    delete[] visited;
+}
+void BFSdisconnected(int **edges, int n)
+{
+    bool *visited = new bool[n];
+    // initializing all nodes univisited
+    for (int i = 0; i < n; i++)
+    {
+        visited[i] = false;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        if (!visited[i])
+        {
+            printBFS(edges, n, i, visited);
+        }
+    }
+    delete[] visited;
+}
+
 int main()
 {
-    int n, e;                                                                                                                                                           
-    cin >> n >> e;                                                                                          
-    int **edges = new int *[n]; // edge matrix                                                                                          
-    for (int i = 0; i < n; i++)                                                                                         
-    {                                                                                           
-        edges[i] = new int[n];                                                                                          
-        for (int j = 0; j < n; j++)                                                                                         
-        {                                                                                           
-            edges[i][j] = 0;                                                                                            
-        }                                                                                           
+    int n, e;
+    cin >> n >> e;
+    int **edges = new int *[n]; // edge matrix
+    for (int i = 0; i < n; i++)
+    {
+        edges[i] = new int[n];
+        for (int j = 0; j < n; j++)
+        {
+            edges[i][j] = 0;
+        }
     }
 
     for (int i = 0; i < e; i++)
@@ -80,34 +111,56 @@ int main()
         visited[i] = false;
     }
 
-    cout<<"DFS"<<endl;
+    cout << "DFS" << endl;
     printDFS(edges, n, 0, visited);
-    cout<<"BFS"<<endl;
-    printBFS(edges, n, 0);
+    cout << "BFS" << endl;
+    printBFS(edges, n, 0, visited);
+
+    // disconnected graphs
+    cout << "DFS" << endl;
+    DFSdisconnected(edges, n);
+    cout << "BFS" << endl;
+    BFSdisconnected(edges, n);
 
     return 0;
 }
 
-//           0       
+//           0
 //          / \      
-//         4   5     
+//         4   5
 //        /     \    
-//       3-------6   
+//       3-------6
 //     /  \       \  
-//    2----1       7 
+//    2----1       7
 
-
-//input : 8
-// 9
-// 0 4
-// 0 5
-// 4 3
-// 3 2
-// 2 1
-// 1 3
-// 5 6
-// 3 6
-// 6 7
+// input : 8
+//  9
+//  0 4
+//  0 5
+//  4 3
+//  3 2
+//  2 1
+//  1 3
+//  5 6
+//  3 6
+//  6 7
 
 // DFS = 0 4 3 1 2 6 5 7
 // BFS = 0 4 5 3 6 1 2 7
+
+// Disconnected Graph
+//       0           1       5
+//     /  \         /       /
+//    2----3       4       6
+//
+// input:-
+// 7
+// 5
+// 0 2
+// 0 3
+// 2 3
+// 1 4
+// 5 6
+//
+//  DFS = 0 2 3 1 4 5 6
+//  BFS = 0 2 3 1 4 5 6
